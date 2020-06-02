f10f3426f856978a5143b335a5dcaabfea895bef
Máté Kaszás <matekaszas@Mates-iMac-Pro.local>
2020-06-02 04:42:25 +0200

    Removed chars that are not in the english alphabet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   require 'checksum_generator'
 
   def index
-
+    
   end
 
   def generate_checksum
       text_to_checksum = ChecksumGenerator.generateChecksum(params[:checksum_text])
       redirect_to root_path(checksum: text_to_checksum)
   end
-  
+
 end
--- lib/checksum_generator.rb
+++ lib/checksum_generator.rb
@@ -1,5 +1,9 @@
 class ChecksumGenerator
     def self.generateChecksum(checksum_text)
-        return "Hello"
+        english_alphabet = ('a'..'z').to_a
+
+        removed_letters = checksum_text.split('').map {|l| l if english_alphabet.include?(l) || l == ' ' }.join('')
+
+        return removed_letters
     end
 end
