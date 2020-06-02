26ddfe7e9e0ef67b565c839229a011c0dab3908a
Máté Kaszás <matekaszas@Mates-iMac-Pro.local>
2020-06-02 04:49:43 +0200

    10 char long words

--- lib/checksum_generator.rb
+++ lib/checksum_generator.rb
@@ -4,6 +4,26 @@ class ChecksumGenerator
 
         removed_letters = checksum_text.split('').map {|l| l if english_alphabet.include?(l) || l == ' ' }.join('')
 
-        return removed_letters
+        ten_char_words = removed_letters.delete(' ').split('')
+        ten_word_array = []
+        num_of_ten_char_words = ten_char_words.length.to_f / 10.to_f
+
+        number_of_new_words = num_of_ten_char_words.ceil
+
+        i = 1
+        if num_of_ten_char_words.to_i === 0
+            ten_word_array.push(ten_char_words.join(''))
+        else
+            while i <= num_of_ten_char_words do
+                ten_word_array.push(ten_char_words[0..9].join(''))
+                ten_char_words.slice!(0..9)
+                i += 1
+            end
+            if ten_char_words.length > 0
+                ten_word_array.push(ten_char_words.join(''))
+            end
+        end
+
+        return ten_word_array.join(' ')
     end
 end
